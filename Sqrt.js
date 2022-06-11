880f6cbd7ee935c6227bd51f7652481d5614063b
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2022-06-11 21:15:28 +0530

    Sqrt

---
+++ Sqrt.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  let left = 0,
+    right = x,
+    ans = 0;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (mid * mid === x) {
+      return mid;
+    } else if (mid * mid < x) {
+      ans = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return ans;
+};
