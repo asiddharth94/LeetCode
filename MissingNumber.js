8a5d8e375e6711b91aea2620265bf2d6953d169a
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2022-06-18 13:29:36 +0530

    Updated Missing Number

--- MissingNumber.js
+++ MissingNumber.js
@@ -1,31 +1,42 @@
-const swap = (arr, first, second) => {
-  let temp = arr[first];
-  arr[first] = arr[second];
-  arr[second] = temp;
-};
+// const swap = (arr, first, second) => {
+//   let temp = arr[first];
+//   arr[first] = arr[second];
+//   arr[second] = temp;
+// };
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var missingNumber = function (nums) {
-  let i = 0;
+  //   let i = 0;
 
-  while (i < nums.length) {
-    if (i === nums[i] || nums[i] === nums.length) {
-      i++;
-    } else {
-      swap(nums, i, nums[i]);
-    }
+  //   while (i < nums.length) {
+  //     if (i === nums[i] || nums[i] === nums.length) {
+  //       i++;
+  //     } else {
+  //       swap(nums, i, nums[i]);
+  //     }
+  //   }
+
+  //   i = 0;
+  //   while (i < nums.length + 1) {
+  //     if (i !== nums[[i]]) {
+  //       return i;
+  //     }
+  //     i++;
+  //   }
+
+  //   return nums.length;
+
+  // using math
+
+  const expectedSum = (nums.length * (nums.length + 1)) / 2;
+  let actualSum = 0;
+
+  for (let num of nums) {
+    actualSum += num;
   }
 
-  i = 0;
-  while (i < nums.length + 1) {
-    if (i !== nums[[i]]) {
-      return i;
-    }
-    i++;
-  }
-
-  return nums.length;
+  return expectedSum - actualSum;
 };
