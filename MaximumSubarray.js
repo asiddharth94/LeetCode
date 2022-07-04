ce56f643b2edf042c712a18ca5fedb4a66511df0
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2022-07-04 12:57:38 +0530

    Maximum Subarray

---
+++ MaximumSubarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  let maxSum = nums[0],
+    currSum = maxSum;
+
+  for (let i = 1; i < nums.length; i++) {
+    currSum = Math.max(currSum + nums[i], nums[i]);
+    maxSum = Math.max(currSum, maxSum);
+  }
+
+  return maxSum;
+};
