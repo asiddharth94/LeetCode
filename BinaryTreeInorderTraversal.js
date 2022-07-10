92b44e487bc0d0492bc61aaf2f4467ad9a06c959
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2022-07-10 19:41:58 +0530

    Binary Tree Inorder Traversal

---
+++ BinaryTreeInorderTraversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const output = [];
+
+  const traverse = (node) => {
+    if (node === null) return;
+
+    traverse(node.left);
+    output.push(node.val);
+    traverse(node.right);
+  };
+
+  traverse(root);
+
+  return output;
+};
